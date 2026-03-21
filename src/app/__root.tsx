4f565bd59163f469bf9fb0bdb1e4e943c64aa370
ppxb <317842449@qq.com>
2026-03-21 17:52:16 +0800

    chore: add demo route

---
+++ src/app/__root.tsx
@@ -0,0 +1,40 @@
+/// <reference types="vite/client" />
+
+import { createRootRoute, HeadContent, Outlet, Scripts } from '@tanstack/react-router'
+import appCss from '@/styles/app.css?url'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      { title: 'TanStack Start Starter' },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  component: RootLayout,
+})
+
+function RootLayout() {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
---
+++ vite.config.mts
@@ -0,0 +1,23 @@
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: 'src',
+      router: {
+        routesDirectory: 'app',
+      },
+    }),
+    viteReact(),
+  ],
+  server: {
+    port: 3000,
+  },
+})
